b887343e880563d1369cbe5cf0dd422790a9f870
Gustav <70881859+gusajer@users.noreply.github.com>
2020-09-07 11:55:49 +0200

    Initial commit

---
+++ src/hello.java
@@ -0,0 +1,10 @@
+public class hello {
+    //psvm
+    public static void main(String[] args) {
+        //sout
+        System.out.println("hello world");
+        System.out.println("hej mor");
+        System.out.println(5);
+
+    }
+}
